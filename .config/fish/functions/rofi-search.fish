b67c6a556ec21fd612617307feeb2ea41b034a70
Denis Duarth <denisduarth30@gmail.com>
2026-06-04 12:05:49 -0300

    Adicionando flags de navegação para rofi e modificando rofi-search.fish

--- .config/fish/functions/rofi-search.fish
+++ .config/fish/functions/rofi-search.fish
@@ -1,11 +1,14 @@
 #!/usr/bin/env fish
 #
-# ~/.local/bin/dmsearch.fish
+# ~/.config/fish/functions/rofi-search.fish
 
 set DMBROWSER "firefox"
 set STATE_FILE /tmp/dmsearch-state
 
-set BANGS "!g"  "!yt"  "!tw"  "!img"  "!ddg"  "!gh"  "!gl"  "!tr"  "!wp"
+set BANGS \
+    "!g"     "!yt"     "!tw"     "!img"     "!ddg"     "!gh"     "!gl"     "!lb"     "!tr"     "!wp"    \
+    "!g-p"   "!yt-p"   "!tw-p"   "!img-p"   "!ddg-p"   "!gh-p"   "!gl-p"   "!lb-p"   "!tr-p"   "!wp-p"
+
 set URLS \
     "https://www.google.com/search?q=" \
     "https://www.youtube.com/results?search_query=" \
@@ -14,48 +17,66 @@ set URLS \
     "https://duckduckgo.com/?q=" \
     "https://github.com/search?q=" \
     "https://gitlab.com/search?search=" \
+    "https://letterboxd.com/search/" \
     "https://translate.google.com/?sl=auto&tl=pt&text=" \
-    "https://pt.wikipedia.org/wiki/"
+    "https://pt.wikipedia.org/wiki/" \
 
 set DEFAULT_URL "https://duckduckgo.com/?q="
 
 if test (count $argv) -eq 0
-    # Primeira chamada: limpa estado e mostra dica
     rm -f $STATE_FILE
     echo "!g !yt !tw !img !ddg !gh !gl !lb !tr !wp"
+
 else
     set input $argv[1]
 
     if test -f $STATE_FILE
-        # Segunda etapa: temos URL salva, input é a query
-        set url (cat $STATE_FILE)
+        set saved (cat $STATE_FILE)
         rm -f $STATE_FILE
-        $DMBROWSER "$url$input"
-    else
-        # Primeira etapa: tenta identificar o bang
+        set url (echo $saved | awk '{print $1}')
+        set private (echo $saved | awk '{print $2}')
+
+        if test "$private" = "true"
+            firefox --private-window "$url$input"
+        else
+            $DMBROWSER "$url$input"
+        end
+
+    else if string match -q '!*' $input
         set bang (echo $input | awk '{print $1}')
         set query (echo $input | cut -d' ' -f2-)
 
+        # Verifica modo privado e obtém o bang base
+        set private false
+        set lookup_bang $bang
+        if string match -q '*-p' $bang
+            set private true
+            set lookup_bang (string replace -- '-p' '' $bang)
+        end
+
+        # Busca a URL nos 10 primeiros bangs
         set url ""
-        for i in (seq 1 (count $BANGS))
-            if test "$bang" = "$BANGS[$i]"
+        for i in (seq 1 10)
+            if test "$lookup_bang" = "$BANGS[$i]"
                 set url $URLS[$i]
                 break
             end
         end
 
-        if test -n "$url"
-            if test -n "$query" -a "$query" != "$bang"
-                # Bang + query na mesma linha: abre direto
-                $DMBROWSER "$url$query"
+        test -z "$url"; and set url $DEFAULT_URL
+
+        if test -n "$query" -a "$query" != "$bang"
+            if test "$private" = "true"
+                firefox --private-window "$url$query"
             else
-                # Só o bang: salva URL e pede a query
-                echo $url > $STATE_FILE
-                echo ""
+                $DMBROWSER "$url$query"
             end
         else
-            # Sem bang: DuckDuckGo direto
-            $DMBROWSER "$DEFAULT_URL$input"
+            echo "$url $private" > $STATE_FILE
+            echo ""
         end
+
+    else
+        $DMBROWSER "$DEFAULT_URL$input"
     end
 end
