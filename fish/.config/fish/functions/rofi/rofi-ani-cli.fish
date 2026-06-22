b33732e02cd5aaab696b6a7ef16cc43e00325c48
Denis Duarth <denisduarth30@gmail.com>
2026-06-22 11:36:34 -0300

    Criando script de seleção de animes via ani-cli com rofi

---
+++ fish/.config/fish/functions/rofi/rofi-ani-cli.fish
@@ -0,0 +1,80 @@
+#!/usr/bin/fish
+
+set STATE_FILE /tmp/rofi-ani-cli-state
+set theme_arg
+if test -f /tmp/rofi-theme-tmp.rasi
+    set theme_arg -theme /tmp/rofi-theme-tmp.rasi
+end
+
+set NAMES \
+    "Pesquisar Anime" \
+    "Baixar Anime" \
+    "Continuar Assistindo" \
+    "Histórico"
+
+set ICONS \
+    "" \
+    "󰇚" \
+    "" \
+    ""
+
+set COMMANDS \
+    "watch" \
+    "download" \
+    "continue" \
+    "history"
+
+set FUNCS_COUNT (count $NAMES)
+
+if test (count $argv) -eq 0
+    printf "\x00prompt\x1f\n"
+    printf "\x00no-custom\x1ftrue\n"
+    for i in (seq 1 $FUNCS_COUNT)
+        printf "%s  %s\x00info\x1f%s\n" $ICONS[$i] $NAMES[$i] $COMMANDS[$i]
+    end
+    exit 0
+end
+
+if test "$argv[1]" = "--run"
+    sleep 0.2
+    test -f $STATE_FILE; or exit 0
+    set cmd (cat $STATE_FILE)
+    rm -f $STATE_FILE
+
+    switch $cmd
+        case watch
+            set query (rofi -dmenu -i -p "" $theme_arg)
+            or exit 0
+            test -n "$query"; or exit 0
+            set q (string escape -- $query)
+            alacritty -e fish -c "ani-cli $q; echo; read -P 'Pressione Enter para fechar...'" &
+            disown
+        case download
+            set query (rofi -dmenu -i -p "󰇚" $theme_arg)
+            or exit 0
+            test -n "$query"; or exit 0
+            set q (string escape -- $query)
+            alacritty -e fish -c "ani-cli -d $q; echo; read -P 'Pressione Enter para fechar...'" &
+            disown
+        case continue
+            alacritty -e fish -c "ani-cli -c; echo; read -P 'Pressione Enter para fechar...'" &
+            disown
+        case history
+            alacritty -e fish -c "ani-cli -H; echo; read -P 'Pressione Enter para fechar...'" &
+            disown
+    end
+    exit 0
+end
+
+set selected (string trim "$argv[1]")
+
+for i in (seq 1 $FUNCS_COUNT)
+    if test "$selected" = (string trim "$ICONS[$i]  $NAMES[$i]")
+        printf "%s" $COMMANDS[$i] > $STATE_FILE
+        fish (realpath (status filename)) --run </dev/null >/dev/null 2>&1 &
+        disown
+        exit 0
+    end
+end
+
+exit 0
